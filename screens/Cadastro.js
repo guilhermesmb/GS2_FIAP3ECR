995eb65fe4e3da1d049b90241b1727dd12d70945
guilhermesmb <guilherme.smbarbosa@gmail.com>
2023-11-18 21:02:35 -0300

    Tudo Ok

--- screens/Cadastro.js
+++ screens/Cadastro.js
@@ -1,18 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Button from '../components/button';
-import Input from '../components/input'
+import Input from '../components/input';
+import axios from 'axios'; // Importe o axios para fazer requisições HTTP
 
-export default function Cadastro() {
+export default function Cadastro({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [nome, setNome] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleCadastro = async () => {
+    try {
+      // Envie uma solicitação POST para o endpoint de cadastro
+      const response = await axios.post('http://localhost:3000/usuarios', {
+        email,
+        nome,
+        senha,
+      });
+
+      // A resposta pode conter informações do usuário recém-criado
+      const novoUsuario = response.data;
+
+      // Agora você pode usar as informações do novo usuário conforme necessário
+      console.log('Novo usuário cadastrado:', novoUsuario);
+
+      // Navegue para a tela de login ou qualquer outra tela necessária
+      navigation.navigate('Login');
+    } catch (error) {
+      console.error('Erro ao cadastrar:', error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={require('../assets/cadastro.jpg')}/>
+      <Image style={styles.image} source={require('../assets/cadastro.jpg')} />
       <Text style={styles.title}>Cadastro de Pacientes</Text>
-      
-      <Input placeholder='E-mail'/>
-      <Input placeholder='Senha' secureTextEntry/>
 
-      <Button onPress={() => navigation.navigate('Login')}>Cadastrar</Button>
+      <Input placeholder="E-mail" onChangeText={setEmail} value={email} />
+      <Input placeholder="Nome" onChangeText={setNome} value={nome} />
+      <Input placeholder="Senha" secureTextEntry onChangeText={setSenha} value={senha} />
+
+      <Button onPress={handleCadastro}>Cadastrar</Button>
       <Button onPress={() => navigation.navigate('Login')}>Voltar</Button>
     </View>
   );
@@ -32,11 +61,10 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 
-image: {
+  image: {
     height: 200,
     width: '100%',
     position: 'absolute',
     top: 0,
-  }
-  
+  },
 });
